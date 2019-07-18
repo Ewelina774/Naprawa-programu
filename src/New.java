b1dda4be93e0d80e3a257f58d434092625272071
Ewelina Hofman <ewelina_hofman98@wp.pl>
2019-07-18 03:52:41 +0200

    Klasa z działaniami matematycznymi

---
+++ src/New.java
@@ -0,0 +1,20 @@
+public class New {
+
+    boolean isEven (int liczba) {
+
+        return liczba%2==0;
+    }
+
+    boolean isOdd (int liczba) {
+        return liczba%2==1;
+    }
+
+    double circleField (double liczba) {
+        return Math.PI*liczba*liczba;
+
+    }
+
+    int power (int liczba) {
+        return liczba*liczba;
+    }
+}
---
+++ src/Testing.java
@@ -0,0 +1,13 @@
+public class Testing {
+
+    public static void main(String[] args) {
+
+        New new1 = new New ();
+        System.out.println(new1.isEven(5));
+        System.out.println(new1.isOdd(9));
+        System.out.println(new1.circleField(6));
+        System.out.println(new1.power(7));
+
+    }
+
+}
