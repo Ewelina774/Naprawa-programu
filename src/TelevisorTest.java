5d22120741c93acce75e2ad636118e0121b285fd
Ewelina Hofman <ewelina_hofman98@wp.pl>
2019-07-18 03:57:00 +0200

    Telewizor on/off

---
+++ src/TelevisorTest.java
@@ -0,0 +1,13 @@
+public class TelevisorTest {
+
+    public static void main(String[] args) {
+        Televisor televisor1 = new Televisor(false);
+
+        televisor1.showStatus();
+        televisor1.turnOn();
+        televisor1.showStatus();
+        televisor1.turnOff();
+        televisor1.showStatus();
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Room room1 = new Room (100, 21, true);
+        room1.startConditioner();
+
+       // System.out.println(startConditioner);
+    }
+}
